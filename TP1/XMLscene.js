8f977faf5e37afe0a9bbd656a20f7863adedddbc
Nunation <oliveiranunopt@gmail.com>
2020-10-08 11:44:05 +0100

    tidying up

--- TP1/XMLscene.js
+++ TP1/XMLscene.js
@@ -142,6 +142,7 @@ class XMLscene extends CGFscene {
         for (var i = 0; i < this.lights.length; i++) {
             this.lights[i].setVisible(true);
             this.lights[i].enable();
+            this.lights[i].update();
         }
 
         if (this.sceneInited) {
