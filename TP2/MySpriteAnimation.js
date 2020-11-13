b3186a14477b6e3590b8dcd7c75f8713ccbf3cf3
Nunation <oliveiranunopt@gmail.com>
2020-11-13 12:04:57 +0000

    classes for spritetext and animation

---
+++ TP2/MySpriteAnimation.js
@@ -0,0 +1,24 @@
+class MySpriteAnimation{
+    constructor(scene,sprites,sizeM,sizeN,duration,startCell,endCell){
+        this.scene=scene;
+        this.sprites=sprites;
+        this.spritesheet = new MySpritesheet(scene,sprites,sizeM,sizeN);
+        this.duration=duration;
+        this.startCell=startCell;
+        this.endCell=endCell;
+        this.rect = new MyRectangle(scene,0,1,1,0);
+    }
+
+    update(time){
+        if(time<=duration*1000){
+            this.spritesheet.activateCellP(this.startCell +  (time/duration)*(this.endCell-startCell));
+        }
+    }
+
+    display(){
+        this.spritesheet.texture.bind();
+        this.scene.setActiveShader(this.spritesheet.spriteShader);
+        this.rect.display();
+        this.scene.setActiveShader(this.scene.defaultShader);
+    }
+}
--- TP2/MySpriteText.js
+++ TP2/MySpriteText.js
@@ -2,14 +2,22 @@ class MySpriteText{
     constructor(scene,text){
         this.scene=scene;
         this.text=text;
-        this.spritesheet = new MySpritesheet();
+        this.spritesheet = new MySpritesheet(scene,"spritesheets/font.png",16,16);
+       
     }
 
     getCharacterPosition(character){
-
-    }
+       position=66; 
+    }  
 
     display(){
-
+        this.spritesheet.texture.bind();
+        this.scene.setActiveShader(this.spritesheet.spriteShader);
+        for(let i=0;i<this.text.length;i++){
+            let rect = new MyRectangle(scene,0+i,1,1+i,0);
+            this.spritesheet.activateCellP(this.getCharacterPosition(this.text[i]));
+            rect.display();
+        }
+        this.scene.setActiveShader(this.scene.defaultShader);
     }
 }
