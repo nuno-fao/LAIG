71c1fa0994c8059cee18f2e135224a8b79805086
Nunation <oliveiranunopt@gmail.com>
2020-11-16 16:07:39 +0000

    optimized stuff, error char for spritetext

--- TP2/MySpriteText.js
+++ TP2/MySpriteText.js
@@ -4,20 +4,32 @@ class MySpriteText{
         this.text=text;
         this.spritesheet = new MySpritesheet(this.scene,"spritesheets/fonte.png",16,14);
         this.offset=text.length/2;
+
+        this.letters = [];
+        for(let i=0;i<this.text.length;i++){
+            let rect = new MyRectangle(this.scene, 0+i - this.offset, -0.5 , 1+i- this.offset ,0.5,1,1);
+            this.letters.push(rect);
+        }
+
     }
 
     getCharacterPosition(character){
-       return character.charCodeAt(0) - 31; 
+        let code =  character.charCodeAt(0);
+        if(code > 31 && code < 256){
+            return code - 31;
+        }
+        else{
+            return 130;
+        }
     }  
 
     display(){
         this.spritesheet.texture.bind(1);
         this.scene.setActiveShaderSimple(this.spritesheet.spriteShader);
-        for(let i=0;i<this.text.length;i++){
-            let rect = new MyRectangle(this.scene, 0+i - this.offset, -0.5 , 1+i- this.offset ,0.5,1,1);
+        for(let i in this.letters){
             this.spritesheet.activateCellP(this.getCharacterPosition(this.text[i]));
             this.spritesheet.updateUniforms();
-            rect.display();
+            this.letters[i].display();
         }
         this.scene.setActiveShader(this.scene.defaultShader);
     }
