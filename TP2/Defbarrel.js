cf403729c1edf5f152b941ffe292207a35715f79
Miguel Pinto <lmiguelapinto@gmail.com>
2020-11-19 15:47:57 +0000

    added full barrel

--- TP2/Defbarrel.js
+++ TP2/Defbarrel.js
@@ -1,52 +1,88 @@
-class Defbarrel extends CGFobject{
+class Defbarrel extends CGFobject {
     constructor(scene, r, R, L, angle, slices, stacks) {
         super(scene);
-        this.r=r;
-        this.R=R;
-        this.L=L;
-        this.angle=angle;
-        this.slices=slices;
-        this.stacks=stacks;
+        this.r = r;
+        this.R = R;
+        this.L = L;
+        this.angle = angle;
+        this.slices = slices;
+        this.stacks = stacks;
         this.initBuffers();
     }
-    initBuffers(){
-        var H = 4/3*(this.R-this.r);
-        var h = 4/3*this.r;
-        this.controlPoints=[
+    initBuffers() {
+        var H = 4 / 3 * (this.R - this.r);
+        var h = 4 / 3 * this.r;
+        this.controlPointsUp = [
             //P4
             [
-                [this.r, 0, 0, 1],    //Q1
-                [this.r + H, 0, H / Math.tan(this.angle), 1],    //Q2
-                [this.r + H, 0, this.L - H / Math.tan(this.angle), 1],    //Q3
-                [this.r, 0, this.L, 1]     //Q4
+                [this.r, 0, 0, 1], //Q1
+                [this.r + H, 0, H / Math.tan(this.angle), 1], //Q2
+                [this.r + H, 0, this.L - H / Math.tan(this.angle), 1], //Q3
+                [this.r, 0, this.L, 1] //Q4
             ],
             //P3
             [
-                [this.r, h, 0, 1],     //Q1
-                [this.r + H, h + H, H / Math.tan(this.angle), 1],     //Q2
-                [this.r + H, h + H, this.L - H / Math.tan(this.angle), 1],      //Q3
-                [this.r, h, this.L, 1]      //Q4
+                [this.r, h, 0, 1], //Q1
+                [this.r + H, h + H, H / Math.tan(this.angle), 1], //Q2
+                [this.r + H, h + H, this.L - H / Math.tan(this.angle), 1], //Q3
+                [this.r, h, this.L, 1] //Q4
             ],
             //P2
             [
-                [-this.r, h, 0, 1],  //Q1
-                [-this.r - H, h + H, H / Math.tan(this.angle), 1],   //Q2
-                [-this.r - H, h + H, this.L - H / Math.tan(this.angle), 1],   //Q3
-                [-this.r, h, this.L, 1]    //Q4
+                [-this.r, h, 0, 1], //Q1
+                [-this.r - H, h + H, H / Math.tan(this.angle), 1], //Q2
+                [-this.r - H, h + H, this.L - H / Math.tan(this.angle), 1], //Q3
+                [-this.r, h, this.L, 1] //Q4
             ],
             //P1
             [
-                [-this.r, 0, 0, 1],  //Q1
-                [-this.r - H, 0, H / Math.tan(this.angle), 1],    //Q2
-                [-this.r - H, 0, this.L - H / Math.tan(this.angle), 1],     //Q2
-                [-this.r, 0, this.L, 1]   //Q4
+                [-this.r, 0, 0, 1], //Q1
+                [-this.r - H, 0, H / Math.tan(this.angle), 1], //Q2
+                [-this.r - H, 0, this.L - H / Math.tan(this.angle), 1], //Q2
+                [-this.r, 0, this.L, 1] //Q4
             ]
 
         ];
-        this.surface = new CGFnurbsSurface(3,3,this.controlPoints);
-        this.object = new CGFnurbsObject(this.scene,this.slices,this.stacks,this.surface);
+
+        this.surfaceUp = new CGFnurbsSurface(3, 3, this.controlPointsUp);
+        this.upSide = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.surfaceUp);
+
+        this.controlPointsDown = [
+            //P1
+            [
+                [-this.r, 0, 0, 1], //Q1
+                [-this.r - H, 0, H / Math.tan(this.angle), 1], //Q2
+                [-this.r - H, 0, this.L - H / Math.tan(this.angle), 1], //Q2
+                [-this.r, 0, this.L, 1] //Q4
+            ],
+            //P2
+            [
+                [-this.r, -h, 0, 1], //Q1
+                [-this.r - H, -(h + H), H / Math.tan(this.angle), 1], //Q2
+                [-this.r - H, -(h + H), this.L - H / Math.tan(this.angle), 1], //Q3
+                [-this.r, -h, this.L, 1] //Q4
+            ],
+            //P3
+            [
+                [this.r, -h, 0, 1], //Q1
+                [this.r + H, -(h + H), H / Math.tan(this.angle), 1], //Q2
+                [this.r + H, -(h + H), this.L - H / Math.tan(this.angle), 1], //Q3
+                [this.r, -h, this.L, 1] //Q4
+            ],
+            //P4
+            [
+                [this.r, 0, 0, 1], //Q1
+                [this.r + H, 0, H / Math.tan(this.angle), 1], //Q2
+                [this.r + H, 0, this.L - H / Math.tan(this.angle), 1], //Q3
+                [this.r, 0, this.L, 1] //Q4
+            ],
+
+        ];
+        this.surfaceDown = new CGFnurbsSurface(3, 3, this.controlPointsDown);
+        this.downSide = new CGFnurbsObject(this.scene, this.slices, this.stacks, this.surfaceDown);
     }
-    display(){
-        this.object.display();
+    display() {
+        this.downSide.display();
+        this.upSide.display();
     }
 }
